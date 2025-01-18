f4744595b8d888a93e9aa45516101b2d72266003
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-01-18 23:12:17 +0800

    added new handlers

--- cmd/main.go
+++ cmd/main.go
@@ -9,11 +9,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello MrJxtr!"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a snippet here"))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form to create snippet here"))
+}
+
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	mux.HandleFunc("/{$}", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
+
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
