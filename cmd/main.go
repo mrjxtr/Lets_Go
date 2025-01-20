ca2b49a9564f424a38252b1a93f4f5ee27da26cd
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-01-20 08:20:58 +0800

    customizing responses

--- cmd/main.go
+++ cmd/main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Server", "GO")
 	w.Write([]byte("Hello MrJxtr!"))
 }
 
@@ -18,8 +19,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	msg := fmt.Sprintf("Display a specific snippet with ID %d...", id)
-	w.Write([]byte(msg))
+	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
 
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
@@ -27,6 +27,7 @@ func snippetCreate(w http.ResponseWriter, r *http.Request) {
 }
 
 func snippetCreatePost(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(201)
 	w.Write([]byte("Save a new snippet here..."))
 }
 
