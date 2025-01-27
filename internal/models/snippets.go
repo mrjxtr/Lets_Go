0fe05f17c79950752394d90ef2ddaafeded69497
Jester Lumacad <mr.jesterlumacad@gmail.com>
2025-01-27 13:12:54 +0800

    Executing SQL: Single-record SQL queries

--- internal/models/snippets.go
+++ internal/models/snippets.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"database/sql"
+	"errors"
 	"time"
 )
 
@@ -25,12 +26,62 @@ func (m *SnippetModel) Insert(
 	content string,
 	expires int,
 ) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+            VALUES(?, ?, DATETIME('now'), DATETIME('now', ?||' days'))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := result.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 // Returns a specific snippet based on id
 func (m *SnippetModel) Get(id int) (Snippet, error) {
-	return Snippet{}, nil
+	// Use strftime to format the datetime as a string in a format Go can parse
+	stmt := `SELECT id, title, content, 
+		strftime('%Y-%m-%d %H:%M:%S', created) as created,
+		strftime('%Y-%m-%d %H:%M:%S', expires) as expires
+		FROM snippets 
+		WHERE expires > datetime('now') AND id = ?`
+
+	// Temporary variables to hold the datetime strings
+	var createdStr, expiresStr string
+	var s Snippet
+
+	err := m.DB.QueryRow(stmt, id).Scan(
+		&s.ID,
+		&s.Title,
+		&s.Content,
+		&createdStr,
+		&expiresStr,
+	)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return Snippet{}, ErrNoRecord
+		}
+		return Snippet{}, err
+	}
+
+	// Parse the datetime strings into time.Time
+	s.Created, err = time.Parse("2006-01-02 15:04:05", createdStr)
+	if err != nil {
+		return Snippet{}, err
+	}
+
+	s.Expires, err = time.Parse("2006-01-02 15:04:05", expiresStr)
+	if err != nil {
+		return Snippet{}, err
+	}
+
+	return s, nil
 }
 
 // Returns the 10 most recently created snippets
